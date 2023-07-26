2b629504b61f1fae233e61b882b20b8209a3246e
Owaji-ibram Nze IDAFIBIA <ibramnze18@gmail.com>
2023-07-26 18:25:01 +0100

    string1.c

---
+++ string1.c
@@ -0,0 +1,83 @@
+#include "shell.h"
+
+/**
+ * _strcpy - Copies string
+ * @dest: dest str
+ * @src: src str
+ * Return: destination pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+/**
+ * _strdup - function taht  duplicates string
+ * @str: duplicate str
+ * Return: Always 0 on success
+ */
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ * _puts - Prints the inputed string
+ * @str: printind string
+ * Return: Always 0 om success
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - writes c to stdout
+ * @c: printing char
+ * Return: On success 1. -1 on erro
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
