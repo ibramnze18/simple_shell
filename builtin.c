60da34e569e27f4d5c1ebc2e0040ba858c9f9045
Owaji-ibram Nze IDAFIBIA <ibramnze18@gmail.com>
2023-07-26 06:24:51 +0100

    builtin.c

---
+++ builtin.c
@@ -0,0 +1,95 @@
+#include "shell.h"
+
+/**
+ * _myexit - A function to exits the shell
+ * @info: function argument
+ * Return: Always 0 on success
+ */
+int _myexit(info_t *info)
+{
+	int exitnum;
+
+	if (info->argv[1])
+	{
+		exitnum = _erratoi(info->argv[1]);
+		if (exitnum == -1)
+		{
+			info->status = 2;
+			print_error(info, "wrong number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+
+/**
+ * _mycd - A func to change the current working directory
+ * @info: struct property
+ * Return: Always 0
+ */
+int _mycd(info_t *info)
+{
+	char *str, *directory, buffer[1024];
+	int chdir_ret;
+
+	str = getcwd(buffer, 1024);
+	if (!str)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		directory = _getenv(info, "HOME=");
+		if (!directory)
+			chdir_ret =
+				chdir((dir = _getenv(info, "PWD=")) ? directory : "/");
+		else
+			chdir_ret = chdir(directory);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(str);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret =
+			chdir((directory = _getenv(info, "OLDPWD=")) ? directory : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * _myhelp - A func that changes the process current directory
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: Always 0
+ */
+int _myhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array);
+	return (0);
+}
+
