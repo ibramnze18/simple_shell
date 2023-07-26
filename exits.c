277e174f81d750bf201b25108d7dc071b63d95d7
Owaji-ibram Nze IDAFIBIA <ibramnze18@gmail.com>
2023-07-26 14:46:25 +0100

    exits.c

---
+++ exits.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+/**
+ **_strncpy - Function to copy a string
+ *@dest: destination
+ *@src: source
+ *@n: num of characters to copy
+ *Return: Aways 0 on success
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - Function to concatenate strings
+ *@dest: first string
+ *@src: second string
+ *@n: num of maximum bytes
+ *Return: Aways 0 on success
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - Character locator in strings
+ *@s: string argument
+ *@c: character to locate
+ *Return: Always 0 on success
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
+
