8d37c46dbf351a8e01069d01101b040fae8b4ab6
Owaji-ibram Nze IDAFIBIA <ibramnze18@gmail.com>
2023-07-26 16:29:06 +0100

    parser.c

---
+++ parser.c
@@ -0,0 +1,84 @@
+#include "shell.h"
+
+/**
+ * is_cmd - checks if a file is an executable
+ * @info: func arg
+ * @path: file path
+ * Return: 1 if true, 0 if false
+ */
+int is_cmd(info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * dup_chars - characters duplicates func
+ * @pathstr: string path
+ * @start: star index to start
+ * @stop: stop index to stop
+ * Return: New buf pointer
+ */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
+}
+
+/**
+ * find_path - Locates comand in the PATH string
+ * @info: func arg
+ * @pathstr: PATH string
+ * @cmd: comand to find
+ * Return: found or NULL
+ */
+char *find_path(info_t *info, char *pathstr, char *cmd)
+{
+	int i = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[i] || pathstr[i] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, i);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathstr[i])
+				break;
+			curr_pos = i;
+		}
+		i++;
+	}
+	return (NULL);
+}
+
